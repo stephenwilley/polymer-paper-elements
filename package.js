37db9824eda70fbdd8f6704f5932996c73b04270
Stephen Willey <sfw@dneg.com>
2016-05-06 15:17:15 -0700

    Updated for fork

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
-  name: 'pixto:polymer-iron-elements',
+  name: 'stephenwilley:polymer-iron-elements',
   version: '0.1.0',
   summary: 'Polymer iron elements',
-  git: 'https://github.com/pixto/polymer-iron-elements',
+  git: 'https://github.com/stephenwilley/polymer-iron-elements',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
-  api.use('pixto:polymer-installer@0.1.0');
+  api.use('stephenwilley:polymer-installer@0.1.0');
   api.addFiles('install.js', 'server');
 });
